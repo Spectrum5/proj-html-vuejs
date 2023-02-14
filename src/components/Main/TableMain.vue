d93a7fd0509e78cfd66a33671debcc7985fd7ebd
Spectrum5 <toskusergio@gmail.com>
2023-02-14 23:48:53 +0100

    Inizio seconda section del main (TableMain)

--- src/components/Main/TableMain.vue
+++ src/components/Main/TableMain.vue
@@ -10,6 +10,26 @@ export default {
 
 <template>
     <!-- creazione template html -->
+    <div class="container">
+        <div class="row">
+            <div class="col-3">
+                <div class="card">
+                    <img src="../../assets/img/anime-fashion.webp" alt="Anime Fashion image">
+                    <img src="../../assets/img/travel-alone.webp" alt="Travel alone image">
+
+                </div>
+            </div>
+            <div class="col-6">
+                <img src="../../assets/img/best-places.webp" alt="">
+            </div>
+            <div class="col-3">
+                <div class="card">
+                    <img src="../../assets/img/rice-ball.webp" alt="rice ball image">
+                    <img src="../../assets/img/visit-france.webp" alt="Visit France image">
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <style scoped lang="scss">
