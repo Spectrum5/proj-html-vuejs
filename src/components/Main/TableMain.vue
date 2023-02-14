4a4acd8047ab9c9dfb7555488d68a6393e371e0d
Spectrum5 <toskusergio@gmail.com>
2023-02-15 00:51:17 +0100

    Aggiornamento TableMain - da sistemare responsive - da aggiungere icone - da sistemare stile generale

--- src/components/Main/TableMain.vue
+++ src/components/Main/TableMain.vue
@@ -1,8 +1,9 @@
 <script>
+import { store } from '../../store.js'
 export default {
     data() {
         return {
-
+            store,
         }
     }
 }
@@ -10,23 +11,52 @@ export default {
 
 <template>
     <!-- creazione template html -->
-    <div class="container">
+    <div class="container pt-5 pb-5">
         <div class="row">
-            <div class="col-3">
-                <div class="card">
-                    <img src="../../assets/img/anime-fashion.webp" alt="Anime Fashion image">
-                    <img src="../../assets/img/travel-alone.webp" alt="Travel alone image">
-
+            <div class="col-3 h-100">
+                <div id="background-img-1" class="card background-filter border-0 rounded d-flex justify-content-end">
+                    <div class="content">
+                        <font-awesome-icon icon="fa-solid fa-user" class="me-2" />{{ store.info[1].demo }}
+                        {{ store.info[1].text }}
+                        <h5>{{ store.info[1].title }}</h5>
+                    </div>
+                </div>
+                <div id="background-img-2"
+                    class="card background-filter border-0 rounded mt-1 d-flex justify-content-end">
+                    <div class="content">
+                        <font-awesome-icon icon="fa-solid fa-user" class="me-2" />{{ store.info[7].demo }}
+                        {{ store.info[7].text }}
+                        <h5>{{ store.info[7].title }}</h5>
+                    </div>
                 </div>
             </div>
             <div class="col-6">
-                <img src="../../assets/img/best-places.webp" alt="">
+                <div id="background-img-3"
+                    class="card border-0 background-filter rounded p-2 d-flex justify-content-end">
+                    <div class="content">
+                        <font-awesome-icon icon="fa-solid fa-user" class="me-2" />{{ store.info[0].demo }}
+                        {{ store.info[0].text }}
+                        <h5>{{ store.info[0].title }}</h5>
+                    </div>
+                </div>
             </div>
             <div class="col-3">
-                <div class="card">
-                    <img src="../../assets/img/rice-ball.webp" alt="rice ball image">
-                    <img src="../../assets/img/visit-france.webp" alt="Visit France image">
+                <div id="background-img-4" class="card background-filter border-0 rounded d-flex justify-content-end">
+                    <div class="content">
+                        <font-awesome-icon icon="fa-solid fa-user" class="me-2" />{{ store.info[6].demo }}
+                        {{ store.info[6].text }}
+                        <h5>{{ store.info[6].title }}</h5>
+                    </div>
                 </div>
+                <div id="background-img-5"
+                    class="card background-filter border-0 rounded mt-1 d-flex justify-content-end">
+                    <div class="content">
+                        <font-awesome-icon icon="fa-solid fa-user" class="me-2" />{{ store.info[3].demo }}
+                        {{ store.info[3].text }}
+                        <h5>{{ store.info[3].title }}</h5>
+                    </div>
+                </div>
+
             </div>
         </div>
     </div>
@@ -35,4 +65,39 @@ export default {
 <style scoped lang="scss">
 /* aggiunta stile css */
 @use '../../styles/partials/variables.scss' as*;
+
+.background-filter {
+    background-size: contain;
+    filter: brightness(70%);
+    text-align: center;
+    color: white;
+}
+
+#background-img-1 {
+    background-image: url('../../assets/img/anime-fashion.webp');
+}
+
+#background-img-2 {
+    background-image: url('../../assets/img/travel-alone.webp');
+}
+
+#background-img-3 {
+    background-image: url('../../assets/img/best-places.webp');
+    height: 100%;
+    background-repeat: no-repeat;
+    background-position: center center;
+    background-size: cover;
+}
+
+#background-img-5 {
+    background-image: url('../../assets/img/visit-france.webp');
+}
+
+#background-img-4 {
+    background-image: url('../../assets/img/healthy-foods.webp');
+}
+
+.card.background-filter {
+    height: 200px;
+}
 </style>
--- src/store.js
+++ src/store.js
@@ -1,5 +1,50 @@
-import { reactive } from 'vue'
- 
+import { reactive } from 'vue';
 export const store = reactive({
-    // message: 'Hello Vite'
-});
+    info: [
+        {
+            demo: 'demo',
+            title: 'Places For a Road Trip',
+            text: 'December 25, 2022'
+        },
+        {
+            demo: 'demo',
+            title: 'Fashion Trend Now A Days',
+            text: 'December 26, 2022'
+        },
+        {
+            demo: 'demo',
+            title: 'Hygenic Receipe To Prepare Rice',
+            text: 'December 26, 2022'
+        },
+        {
+            demo: 'demo',
+            title: 'The Best Healthy Foods In 2022',
+            text: 'December 26, 2022'
+        },
+        {
+            demo: 'demo',
+            title: 'The Best Winter Outfits',
+            text: 'December 26, 2022'
+        },
+        {
+            demo: 'demo',
+            title: 'Beginner Photographer\'s Mistakes',
+            text: 'December 26, 2022'
+        },
+        {
+
+            title: 'Music The Love Of My Life',
+            text: 'December 26, 2022'
+        },
+        {
+            demo: 'demo',
+            title: 'Travelling Alone Is Awesome',
+            text: 'December 26, 2022'
+        },
+        {
+            demo: 'demo',
+            title: 'The best Succes Stories',
+            text: 'December 26, 2022'
+        },
+    ]
+})
