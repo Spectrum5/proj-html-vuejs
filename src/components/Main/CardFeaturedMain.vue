747f0a2f84c006a951da619e0ef2c54ab5b59b46
Spectrum5 <toskusergio@gmail.com>
2023-02-15 17:24:45 +0100

    Modificato slider mettendo -for - da sistemare immagine - spostare i button - far funzionare i button

--- src/components/Main/CardFeaturedMain.vue
+++ src/components/Main/CardFeaturedMain.vue
@@ -40,7 +40,7 @@ export default {
                     imageUrl: '../../assets/img/best-places.webp',
                     altText: '...'
                 },
-
+                
             ]
         }
     },
@@ -62,92 +62,25 @@ export default {
             <div class="col">
                 <div id="carouselExampleControls" class="carousel" data-bs-ride="carousel">
                     <div class="carousel-inner">
-                        <div class="carousel-item active">
+                        <div v-for="(card, index) in cards" :key="index"
+                            :class="['carousel-item', { active: index === currentSlide }]">
                             <div class="card">
-                                <div class="img-wrapper"><img src="../../assets/img/best-places.webp"
-                                        class="d-block w-100" alt="..."> </div>
-                                <div class="card-body">
-                                    <h5 class="card-title">Card title 1</h5>
-                                    <p class="card-text">Some quick example text to build on the card title and make up
-                                        the bulk of the
-                                        card's content.</p>
-                                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                                <div class="img-wrapper">
+                                    <img :src="card.imageUrl" class="d-block w-100" :alt="card.altText">
                                 </div>
-                            </div>
-                        </div>
-                        <div class="carousel-item">
-                            <div class="card">
-                                <div class="img-wrapper"><img src="../../assets/img/best-places.webp"
-                                        class="d-block w-100" alt="..."> </div>
                                 <div class="card-body">
-                                    <h5 class="card-title">Card title 2</h5>
-                                    <p class="card-text">Some quick example text to build on the card title and make up
-                                        the bulk of the
-                                        card's content.</p>
-                                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="carousel-item">
-                            <div class="card">
-                                <div class="img-wrapper"><img src="../../assets/img/best-places.webp"
-                                        class="d-block w-100" alt="..."> </div>
-                                <div class="card-body">
-                                    <h5 class="card-title">Card title 3</h5>
-                                    <p class="card-text">Some quick example text to build on the card title and make up
-                                        the bulk of the
-                                        card's content.</p>
-                                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="carousel-item">
-                            <div class="card">
-                                <div class="img-wrapper"><img src="../../assets/img/best-places.webp"
-                                        class="d-block w-100" alt="..."> </div>
-                                <div class="card-body">
-                                    <h5 class="card-title">Card title 4</h5>
-                                    <p class="card-text">Some quick example text to build on the card title and make up
-                                        the bulk of the
-                                        card's content.</p>
-                                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="carousel-item">
-                            <div class="card">
-                                <div class="img-wrapper"><img src="../../assets/img/best-places.webp"
-                                        class="d-block w-100" alt="..."> </div>
-                                <div class="card-body">
-                                    <h5 class="card-title">Card title 5</h5>
-                                    <p class="card-text">Some quick example text to build on the card title and make up
-                                        the bulk of the
-                                        card's content.</p>
-                                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="carousel-item">
-                            <div class="card">
-                                <div class="img-wrapper"><img src="../../assets/img/best-places.webp"
-                                        class="d-block w-100" alt="..."> </div>
-                                <div class="card-body">
-                                    <h5 class="card-title">Card title 6</h5>
-                                    <p class="card-text">Some quick example text to build on the card title and make up
-                                        the bulk of the
-                                        card's content.</p>
+                                    <h5 class="card-title">{{ card.title }}</h5>
+                                    <p class="card-text">{{ card.body }}</p>
                                     <a href="#" class="btn btn-primary">Go somewhere</a>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"
-                        data-bs-slide="prev">
+                    <button class="carousel-control-prev" type="button" @click="prevSlide">
                         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                         <span class="visually-hidden">Previous</span>
                     </button>
-                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls"
-                        data-bs-slide="next">
+                    <button class="carousel-control-next" type="button" @click="nextSlide">
                         <span class="carousel-control-next-icon" aria-hidden="true"></span>
                         <span class="visually-hidden">Next</span>
                     </button>
