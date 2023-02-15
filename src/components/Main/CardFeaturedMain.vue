36b2df22c79988d80cbcc26e9ce3892f908bee72
Spectrum5 <toskusergio@gmail.com>
2023-02-15 20:26:22 +0100

    Prova numero 3 di CardFeaturedMain

--- src/components/Main/CardFeaturedMain.vue
+++ src/components/Main/CardFeaturedMain.vue
@@ -2,141 +2,172 @@
 export default {
     data() {
         return {
-            currentSlide: 0,
             cards: [
                 {
-                    title: 'Card title 1',
-                    body: 'Some quick example text to build on the card title and make up the bulk of the card\'s content.',
-                    imageUrl: '../../assets/img/best-places.webp',
-                    altText: '...'
+                    image: "../../assets/img/best-places.webp",
+                    title: "Card Title 1",
+                    description: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+                    link: "#"
                 },
                 {
-                    title: 'Card title 2',
-                    body: 'Some quick example text to build on the card title and make up the bulk of the card\'s content.',
-                    imageUrl: '../../assets/img/best-places.webp',
-                    altText: '...'
+                    image: "../../assets/img/best-places.webp",
+                    title: "Card Title 2",
+                    description: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+                    link: "#"
                 },
                 {
-                    title: 'Card title 3',
-                    body: 'Some quick example text to build on the card title and make up the bulk of the card\'s content.',
-                    imageUrl: '../../assets/img/best-places.webp',
-                    altText: '...'
+                    image: "../../assets/img/best-places.webp",
+                    title: "Card Title 3",
+                    description: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+                    link: "#"
                 },
                 {
-                    title: 'Card title 3',
-                    body: 'Some quick example text to build on the card title and make up the bulk of the card\'s content.',
-                    imageUrl: '../../assets/img/best-places.webp',
-                    altText: '...'
+                    image: "../../assets/img/best-places.webp",
+                    title: "Card Title 4",
+                    description: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+                    link: "#"
                 },
                 {
-                    title: 'Card title 3',
-                    body: 'Some quick example text to build on the card title and make up the bulk of the card\'s content.',
-                    imageUrl: '../../assets/img/best-places.webp',
-                    altText: '...'
+                    image: "../../assets/img/best-places.webp",
+                    title: "Card Title 5",
+                    description: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+                    link: "#"
                 },
                 {
-                    title: 'Card title 3',
-                    body: 'Some quick example text to build on the card title and make up the bulk of the card\'s content.',
-                    imageUrl: '../../assets/img/best-places.webp',
-                    altText: '...'
+                    image: "../../assets/img/best-places.webp",
+                    title: "Card Title 6",
+                    description: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+                    link: "#"
                 },
-                
-            ]
+                // add more cards here
+            ],
+            innerStyles: {},
+            step: '',
+            transitioning: false
         }
     },
+    mounted() {
+        this.setStep()
+        this.resetTranslate()
+    },
     methods: {
-        prevSlide() {
-            this.currentSlide = (this.currentSlide - 1 + this.cards.length) % this.cards.length;
+        setStep() {
+            const innerWidth = this.$refs.inner.scrollWidth
+            const totalCards = this.cards.length
+            this.step = `${innerWidth / totalCards}px`
         },
-        nextSlide() {
-            this.currentSlide = (this.currentSlide + 1) % this.cards.length;
+        next() {
+            if (this.transitioning) return
+            this.transitioning = true
+            this.moveLeft()
+            this.afterTransition(() => {
+                const card = this.cards.shift()
+                this.cards.push(card)
+                this.resetTranslate()
+                this.transitioning = false
+            })
+        },
+        prev() {
+            if (this.transitioning) return
+            this.transitioning = true
+            this.moveRight()
+            this.afterTransition(() => {
+                const card = this.cards.pop()
+                this.cards.unshift(card)
+                this.resetTranslate()
+                this.transitioning = false
+            })
+        },
+        moveLeft() {
+            this.innerStyles = {
+                transform: `translateX(-${this.step})
+                    translateX(-${this.step})`
+            }
+        },
+        moveRight() {
+            this.innerStyles = {
+                transform: `translateX(${this.step})
+                    translateX(-${this.step})`
+            }
+        },
+        afterTransition(callback) {
+            const listener = () => {
+                callback()
+                this.$refs.inner.removeEventListener('transitionend', listener)
+            }
+            this.$refs.inner.addEventListener('transitionend', listener)
+        },
+        resetTranslate() {
+            this.innerStyles = {
+                transition: 'none',
+                transform: `translateX(-${this.step})`
+            }
         }
+    },
+    name: 'Carousel',
+    components: {
+
     }
 }
 </script>
 
 <template>
-    <!-- creazione template html -->
-    <div class="container">
-        <div class="row">
-            <div class="col">
-                <div id="carouselExampleControls" class="carousel" data-bs-ride="carousel">
-                    <div class="carousel-inner">
-                        <div v-for="(card, index) in cards" :key="index"
-                            :class="['carousel-item', { active: index === currentSlide }]">
-                            <div class="card">
-                                <div class="img-wrapper">
-                                    <img :src="card.imageUrl" class="d-block w-100" :alt="card.altText">
-                                </div>
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="col-6">
+                    <div>
+                        <h2>Portfolio</h2>
+                    </div>
+                </div>
+                <div class="col-6 d-flex justify-content-end">
+                    <button @click="prev">prev</button>
+                    <button @click="next">next</button>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <div class="carousel">
+                        <div class="inner" ref="inner" :style="innerStyles">
+                            <div class="card" v-for="(card, index) in cards" :key="index">
+                                <img :src="card.image" class="card-img-top" alt="card image">
                                 <div class="card-body">
                                     <h5 class="card-title">{{ card.title }}</h5>
-                                    <p class="card-text">{{ card.body }}</p>
-                                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                                    <p class="card-text">{{ card.description }}</p>
+                                    <a :href="card.link" class="btn btn-primary">Go somewhere</a>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <button class="carousel-control-prev" type="button" @click="prevSlide">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Previous</span>
-                    </button>
-                    <button class="carousel-control-next" type="button" @click="nextSlide">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Next</span>
-                    </button>
                 </div>
             </div>
         </div>
-    </div>
+    </section>
 </template>
 
-<style scoped lang="scss">
-/* aggiunta stile css */
-@use '../../styles/partials/variables.scss' as*;
+<style lang="scss" scoped>
+.carousel {
+    width: 100%;
+    overflow: hidden;
+}
+
+.inner {
+    transition: transform 1s;
+    white-space: nowrap;
+}
 
 .card {
-    margin: 0 0.5em;
-    box-shadow: 2px 6px 8px 0 rgba(22, 22, 26, 0.18);
-    border: none;
+    width: calc(100% / 3);
+    display: inline-flex;
 }
 
-.carousel-control-prev,
-.carousel-control-next {
-    background-color: #e1e1e1;
-    width: 6vh;
-    height: 6vh;
-    border-radius: 50%;
-    top: 50%;
-    transform: translateY(-50%);
-}
-
-@media (min-width: 768px) {
-    .carousel-item {
-        margin-right: 0;
-        flex: 0 0 33.333333%;
-        display: block;
-    }
-
-    .carousel-inner {
-        display: flex;
-    }
-}
-
-.card .img-wrapper {
-    max-width: 100%;
-    height: 13em;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-}
-
-.card img {
-    max-height: 100%;
-}
-
-@media (max-width: 767px) {
-    .card .img-wrapper {
-        height: 17em;
-    }
+/* optional */
+button {
+    margin-right: 5px;
+    margin-top: 10px;
 }
 </style>
