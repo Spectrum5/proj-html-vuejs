7145d14d23a0b54dae6ca1981196f8f0c8d7c295
Spectrum5 <toskusergio@gmail.com>
2023-02-16 05:45:18 +0100

    Ridimensionato player youtube

--- src/components/Main/YoutubeMain.vue
+++ src/components/Main/YoutubeMain.vue
@@ -20,7 +20,7 @@ export default {
     <div class="container">
         <div class="row my-5">
             <div class="col d-flex justify-content-center">
-                <iframe id="ytplayer" type="text/html" width="1300" height="675"
+                <iframe id="ytplayer" type="text/html" height="600"
                     src="https://www.youtube.com/embed/?listType=playlist&list=PLzSjbEiFKZ_w9zWXjVSTLi5FUlcPHwQCc&autoplay=1"
                     frameborder="0" allowfullscreen>
                 </iframe>
@@ -33,5 +33,7 @@ export default {
 /* aggiunta stile css */
 @use '../../styles/partials/variables.scss' as*;
 
-
+#ytplayer {
+    width: 100%;
+}
 </style>
