594fa5fdd827df33eb80dc9f5f6d719a6add20af
Spectrum5 <toskusergio@gmail.com>
2023-02-16 08:40:01 +0100

    Aggiunto category su store.js

--- src/store.js
+++ src/store.js
@@ -4,47 +4,56 @@ export const store = reactive({
         {
             demo: 'Demo',
             title: 'Places For a Road Trip',
-            text: 'December 25, 2022'
+            text: 'December 25, 2022',
+            category: 'Lifestyle',
         },
         {
             demo: 'Demo',
             title: 'Fashion Trend Now A Days',
-            text: 'December 26, 2022'
+            text: 'December 26, 2022',
+            category: 'Fashion'
         },
         {
             demo: 'Demo',
             title: 'Hygenic Receipe To Prepare Rice',
-            text: 'December 26, 2022'
+            text: 'December 26, 2022',
+            category: 'Food'
         },
         {
             demo: 'Demo',
             title: 'The Best Healthy Foods In 2022',
-            text: 'December 26, 2022'
+            text: 'December 26, 2022',
+            category: 'Food'
         },
         {
             demo: 'Demo',
             title: 'The Best Winter Outfits',
-            text: 'December 26, 2022'
+            text: 'December 26, 2022',
+            category: 'Fashion'
         },
         {
             demo: 'Demo',
             title: 'Beginner Photographer\'s Mistakes',
-            text: 'December 26, 2022'
+            text: 'December 26, 2022',
+            category: 'Fashion'
         },
         {
             demo: 'Demo',
             title: 'Music The Love Of My Life',
-            text: 'December 25, 2022'
+            text: 'December 25, 2022',
+            category: 'Culture'
         },
         {
             demo: 'Demo',
             title: 'Travelling Alone Is Awesome',
-            text: 'December 26, 2022'
+            text: 'December 26, 2022',
+            category: 'Travel'
         },
         {
             demo: 'Demo',
             title: 'The best Succes Stories',
-            text: 'December 26, 2022'
+            text: 'December 26, 2022',
+            category: 'Culture',
         },
     ]
 })
